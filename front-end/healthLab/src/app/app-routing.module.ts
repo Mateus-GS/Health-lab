c5a61f63a52ca8e4f9d395311c53fd20cce64544
Mateus Goncalves <mateusgsbh2@gmail.com>
2021-05-20 21:19:54 -0300

    alterando rota inicial

--- front-end/healthLab/src/app/app-routing.module.ts
+++ front-end/healthLab/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { PrincipalComponent } from './pages/principal/principal.component';
 import { RecuperarSenhaComponent } from './pages/recuperar-senha/recuperar-senha.component';
 
 const routes: Routes = [
-  { path:"login", component: LoginComponent},
+  { path:"", component: LoginComponent},
   { path:"cadastro", component: CadastroComponent},
   { path:"recuperar-senha", component: RecuperarSenhaComponent},
   { path:"dashboard", component: PrincipalComponent},
